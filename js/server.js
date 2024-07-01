086c81cbf0c371739beaa6c14fb61328a97cc122
Gal Shimer <galshimer2@gmail.com>
2024-07-01 15:43:25 +0300

    add CRUDL

---
+++ js/server.js
@@ -0,0 +1,69 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
+
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello There!')
+})
+
+
+app.get('/api/bug', (req, res) => {
+    const filterBy = {
+        txt: req.query.txt,
+        severity: +req.query.severity
+    }
+    bugService.query(filterBy)
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error('Cannot get bugs', err)
+            res.status(500).send('Cannot get bugs')
+        })
+})
+
+app.get('/api/bug/save', (req, res) => {
+    const bugToSave = {
+        _id: req.query._id,
+        title: req.query.title,
+        description: req.query.description,
+        severity: +req.query.severity,
+        createdAt: Date.now()
+    }
+
+    bugService.save(bugToSave)
+        .then(bug => res.send(bug))
+        .catch((err) => {
+            loggerService.error('Cannot save bug', err)
+            res.status(500).send('Cannot save bug', err)
+        })
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+    console.log('req.params:', req.params)
+    const { bugId } = req.params
+    console.log('bugId:', bugId)
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch((err) => {
+            loggerService.error('Cannot get bug', err)
+            res.status(500).send('Cannot get bug')
+        })
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    // console.log('req.params:', req.params)
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(`Bug (${bugId}) removed!`))
+        .catch((err) => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(500).send('Cannot remove bug', err)
+        })
+
+})
+
+
+
+app.listen(3030, () => console.log('Server ready at port 3030!'))
---
+++ js/services/bug.service.js
@@ -0,0 +1,63 @@
+import { utilService } from "./util.service.js"
+import fs from 'fs'
+
+
+const bugs = utilService.readJsonFile('data/bug.json')
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+function query(filterBy = {}) {
+    return Promise.resolve(bugs)
+        .then(bugs => {
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                bugs = bugs.filter(bug => regExp.test(bugs.title))
+            }
+            if (filterBy.severity) {
+                bugs = bugs.filter(bug => bug.severity >= filterBy.severity)
+            }
+            return bugs
+        })
+}
+
+function getById(bugId) {
+    const bug = bugs.find(bug => bug._id === bugId)
+    if (!bug) return Promise.reject('Cannot find bug - ' + bugId)
+    return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+    const bugIdx = bugs.findIndex(bug => bug._id === bugId)
+    if (bugIdx < 0) return Promise.reject('Cannot find bug - ' + bugId)
+    bugs.splice(bugIdx, 1)
+    return _saveBugsToFile().then(() => `bug (${bugId}) removed!`)
+}
+
+function save(bugToSave) {
+    if (bugToSave._id) {
+        const bugIdx = bugs.findIndex(bug => bug._id === bugToSave._id)
+        bugs[bugIdx] = bugToSave
+    } else {
+        bugToSave._id = utilService.makeId()
+        bugs.unshift(bugToSave)
+    }
+
+    return _saveBugsToFile().then(() => bugToSave)
+}
+
+function _saveBugsToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(bugs, null, 4)
+        fs.writeFile('data/bug.json', data, (err) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
